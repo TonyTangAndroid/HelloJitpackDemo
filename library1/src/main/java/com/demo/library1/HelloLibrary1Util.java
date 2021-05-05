68753332b436142aa71786d92239eb3953d63427
TonyTandAndroid <tonythompsoncmu@gmail.com>
2021-05-05 13:19:55 +0800

    使用app implementation 集成 library1 和 library2

--- library1/src/main/java/com/demo/library1/HelloLibrary1Util.java
+++ library1/src/main/java/com/demo/library1/HelloLibrary1Util.java
@@ -1,8 +1,15 @@
 package com.demo.library1;
 
+import com.demo.library2.HelloLibrary2Util;
+
 public class HelloLibrary1Util {
 
   public static String message(String name) {
     return "HelloLibrary1Util + " + name;
+  }
+
+
+  public static String messageFromLibrary2(String name) {
+    return HelloLibrary2Util.message(name);
   }
 }
--- library2/src/main/java/com/demo/library2/HelloLibrary2Util.java
+++ library2/src/main/java/com/demo/library2/HelloLibrary2Util.java
@@ -5,4 +5,6 @@ public class HelloLibrary2Util {
   public static String message(String name) {
     return "HelloLibrary2Util + " + name;
   }
+
+
 }
