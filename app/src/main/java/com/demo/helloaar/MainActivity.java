6bf6eb17144e60c324545924d7a6517dd5b0d767
TonyTandAndroid <tonythompsoncmu@gmail.com>
2021-05-05 13:16:24 +0800

    集成 library2

--- app/src/main/java/com/demo/helloaar/MainActivity.java
+++ app/src/main/java/com/demo/helloaar/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import com.demo.library1.HelloLibrary1Util;
+import com.demo.library2.HelloLibrary2Util;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,5 +14,6 @@ public class MainActivity extends AppCompatActivity {
     setContentView(R.layout.activity_main);
     TextView textView = findViewById(R.id.tv_hello);
     textView.setText(HelloLibrary1Util.message("tony"));
+    textView.setText(HelloLibrary2Util.message("tony"));
   }
 }
